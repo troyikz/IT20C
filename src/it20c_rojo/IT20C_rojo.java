5eab526656358236d7e5e68f12ab6a620d94ee71
troyikz <rojotroy40@gmail.com>
2024-09-18 16:47:16 +0800

    initial commit

---
+++ src/it20c_rojo/IT20C_rojo.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it20c_rojo;
+
+/**
+ *
+ * @author ITLAB2-PC14-STUDENT
+ */
+public class IT20C_rojo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // TODO code application logic here/*
+
+        int [][] hayst = {
+            {0,3,6},
+            {1,4,7},
+            {2,5,8}
+        };
+        for (int i=0;i<hayst.length;i++) {
+            for (int j=0;j<hayst[i].length;j++) {
+                System.out.print(hayst[i][j]+" ");
+
+            }
+            System.out.println();
+         }
+    }
+}
+
